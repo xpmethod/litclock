67f29d02bbe3e47496f9bb01e2a5462c39528e08
denten <denten@gmail.com>
2014-03-12 13:03:08 -0400

    add a scheduler

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
 import csv
 import time
 import datetime
+import threading
 
-# open takes the name of the file and modes like read and write
-with open('test.csv', 'r') as csvfile:
-    tweetreader = csv.DictReader(csvfile, delimiter=",")
+def tweetbot():
+    # open takes the name of the file and modes like read and write
+    with open('tweets.csv', 'r') as csvfile:
+        tweetreader = csv.DictReader(csvfile, delimiter=",")
 
-    for row in tweetreader:
-        ft = row['time']
-        hour = int(ft.split(':')[0])
-        minute = int(ft.split(':')[1])
-        now = datetime.datetime.now().time()
-        # import pdb; pdb.set_trace();
-        if hour == now.hour and minute == now.minute:
-            print row['text']
+        for row in tweetreader:
+            ft = row['time']
+            hour = int(ft.split(':')[0])
+            minute = int(ft.split(':')[1])
+            now = datetime.datetime.now().time()
+            if hour == now.hour and minute == now.minute:
+                print row['text']
+
+    threading.Timer(60, tweetbot).start()
+
+tweetbot()
