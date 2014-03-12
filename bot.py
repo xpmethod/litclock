bf797633b9b33a5bd135e8b85e9377543161c577
denten <denten@gmail.com>
2014-03-11 22:19:55 -0400

    adding time logic

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
 import csv
+import time
+import datetime
+import pdb
 
 # open takes the name of the file and modes like read and write
 with open('tweets.csv', 'r') as csvfile:
-    tweetreader = csv.reader(csvfile, delimiter=",")
+    tweetreader = csv.DictReader(csvfile, delimiter=",")
     for row in tweetreader:
-        print row
+        ft = row['time']
+        hour = ft.split(':')[0]
+        minute = ft.split(':')[1]
+        now = datetime.datetime.now().time()
+        if hour == now.hour and minute == now.minute:
+            pdb.set_trace()
+            print row['text']
