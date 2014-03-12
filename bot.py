71d8c0d6926e80e4164faec9332ec6cbbc1fb4fc
denten <denten@gmail.com>
2014-03-11 23:41:52 -0400

    script works

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 import csv
 import time
 import datetime
-import pdb
 
 # open takes the name of the file and modes like read and write
-with open('tweets.csv', 'r') as csvfile:
+with open('test.csv', 'r') as csvfile:
     tweetreader = csv.DictReader(csvfile, delimiter=",")
+
     for row in tweetreader:
         ft = row['time']
-        hour = ft.split(':')[0]
-        minute = ft.split(':')[1]
+        hour = int(ft.split(':')[0])
+        minute = int(ft.split(':')[1])
         now = datetime.datetime.now().time()
+        # import pdb; pdb.set_trace();
         if hour == now.hour and minute == now.minute:
-            pdb.set_trace()
             print row['text']
---
+++ million.md
@@ -0,0 +1,1 @@
+We have no interest in Twitter. Twitter bots howeve, are a whole other story. 
