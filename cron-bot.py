d361222030ce4730e9df81a093c987cd38c9756d
denten <denten@gmail.com>
2014-03-18 17:06:56 -0400

    clean up whitespace

--- cron-bot.py
+++ cron-bot.py
@@ -8,53 +8,39 @@ import tweepy
 # paths have to be explicit for cron
 path = '/home/shared-drives/litclock/'
 
-
 # separate the credits out to keep out of github
 def getcreds():
     with open(path + 'creds.csv', 'r') as csvfile:
         creds = csv.DictReader(csvfile, delimiter=",")
-
         row = creds.next()
-
         return row['token'], row['secret'], row['akey'], row['asecret']
 
-
-# get the text corresponding to current time
 def gettext():
-
     now = datetime.datetime.now().time()
-
     with open(path + 'tweets.csv', 'r') as csvfile:
         tweets = csv.DictReader(csvfile, delimiter=",")
-
         for row in tweets:
-
             ft = row['time']
             hour = int(ft.split(':')[0])
             minute = int(ft.split(':')[1])
             if hour == now.hour and minute == now.minute:
                 return row['text']
 
-
-# tweet
 def tweet(k, t):
-
     try:
         # k stores token, secret, api key, and api secret
         auth = tweepy.OAuthHandler(k[2], k[3])
         auth.set_access_token(k[0], k[1])
         api = tweepy.API(auth)
         api.update_status(t)
-
         # dummy write to a file instead of tweeting
         # f = open(path + 'log.txt', 'a')
         # f.write(t + '\n')
-
     except tweepy.error.TweepError, e:
+        # implement logging later
         # print 'failed because of %s' % e.reason
         pass
 
-# get creds > get text > tweet
 keys = getcreds()
 text = gettext()
 tweet(keys, text)
